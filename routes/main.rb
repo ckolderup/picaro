2da8bd89cc7b74e23c9567eff542f0eb3930af7c
Casey Kolderup <casey@kolderup.org>
2012-02-22 23:07:58 -0500

    first stab at /play haml port

--- routes/main.rb
+++ routes/main.rb
@@ -4,7 +4,7 @@ end
 
 get '/play/:game_id' do
   if File.exist?(Picaro.public_folder + '/game_data/' + params[:game_id].to_s + '.json')
-    @game_id = params[:game_id]
+    game_id = params[:game_id]
   end
-  erb :play
+  haml :play, :layout => false, :locals => { :game_id => game_id }
 end
